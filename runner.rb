e241a70f1bd101f087a340551263268b0163f651
Karyatis <anna.p212@gmail.com>
2020-10-29 21:19:29 -0500

    Basic functionality complete

--- runner.rb
+++ runner.rb
@@ -25,9 +25,32 @@ round_data.each do |q|
 	potential_answers << new_question.shuffle
 end
 
+correct_answers = []
+
+round_data.each do |q|
+	correct_answers << q.last.downcase
+end
+
+puts "Let's get started!"
+sleep(0.8)
+
+score = 0
+
 i = 0
 until i == 10
-	p questions[i]
+
+	puts questions[i]
   puts potential_answers[i]
+  answer = gets.chomp.downcase
+  
+  if correct_answers.include?(answer)
+  	score += 1
+  	puts "\u{1f600}"
+  else
+  	puts "\u{1F629}"
+  end
   i += 1
+
 end
+
+puts "Your score is #{score}"
