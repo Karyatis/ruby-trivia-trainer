07c9da9ff7fde74245d0d75797215373d2ce76dd
Karyatis <anna.p212@gmail.com>
2020-10-31 14:49:45 -0500

    Pull functions into round.rb

--- round.rb
+++ round.rb
@@ -1,40 +1,45 @@
-require 'json'
-
 module Round
 
+	def round_data(questions_array)
+		round_questions = []
+		round_questions << questions_array.sample(10)
+		round_questions = round_questions.flatten
+		round_data = round_questions.map {|question| [question["question"], question["incorrect"], question["correct"]]}
+		round_data
+	end
 
-  data = File.read("data.json")
-
-  questions_array = JSON.parse(data)
-
-
-	#Pick 10 random questions
-	round_questions = []
+	def questions(round_data)
+		questions = []
+		round_data.each do |q|
+			questions << q[0]
+		end
+		questions
+	end
   
-	round_questions << questions_array.sample(10)
-
-	round_questions = round_questions.flatten
-	  
-	round_data = round_questions.map {|question| [question["question"], question["incorrect"], question["correct"]]}
-
-	questions = []
-
-	round_data.each do |q|
-		questions << q[0]
+	def potential_answers(round_data)
+		potential_answers = []
+		round_data.each do |q|
+			new_question = q[1] << q[2] 
+			potential_answers << new_question.shuffle
+		end
+		potential_answers
 	end
 
-	potential_answers = []
-
-	round_data.each do |q|
-		new_question = q[1] << q[2] 
-		potential_answers << new_question.shuffle
+	def correct_answers(round_data)
+		correct_answers = []
+		round_data.each do |q|
+			correct_answers << q.last.downcase
+		end
+		correct_answers
 	end
 
-	correct_answers = []
-
-	round_data.each do |q|
-		correct_answers << q.last.downcase
-	end
-
-	p round_data
+	def score_text(score)
+	 if score >= 8
+	    puts "Good job!"
+	    puts "Your score is #{score}"
+	  else
+	    puts "Better luck next time!"
+	    puts "Your score is #{score}"
+	  end
+	end 
 end
--- runner.rb
+++ runner.rb
@@ -1,25 +1,39 @@
 require_relative 'round'  
+require 'json'
+include Round
 
-puts "Let's get started!"
-sleep(0.8)
+  data = File.read("data.json")
 
-score = 0
+  data_array = JSON.parse(data)
 
-i = 0
-until i == 10
+  question_selection = round_data(data_array)
 
-	puts questions[i]
-  puts potential_answers[i]
-  answer = gets.chomp.downcase
-  
-  if correct_answers.include?(answer)
-  	score += 1
-  	puts "\u{1f600}"
-  else
-  	puts "\u{1F629}"
+  questions = questions(question_selection)
+  potential_answers = potential_answers(question_selection)
+  correct_answers = correct_answers(question_selection)
+
+  puts "Let's get started!"
+  sleep(0.8)
+
+  score = 0
+
+  i = 0
+  until i == 10
+
+  	puts questions[i]
+    puts potential_answers[i]
+    answer = gets.chomp.downcase
+    
+    if correct_answers.include?(answer)
+    	score += 1
+    	puts "\u{1f600}"
+    else
+    	puts "\u{1F629}"
+    end
+    i += 1
   end
-  i += 1
 
-end
-
-puts "Your score is #{score}"
+  puts "Ready for your score?"
+  sleep(0.8)
+  
+  score_text(score)
